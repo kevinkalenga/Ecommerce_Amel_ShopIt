7e4d459949c6851c9e4c90d0097301b08b8cb4fe
kevin kalenga <kevinkalenga10@gmail.com>
2025-10-22 16:28:54 +0200

    Creation de premiere route

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config({path: 'backend/config/config.env'})
+import productRoutes from "./routes/productRoutes.js"
 
 const app = express();
+
+
+app.use('/api/v1', productRoutes)
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Le serveur est lancé sur le port : ${process.env.PORT} dans ${process.env.NODE_ENV}`)
