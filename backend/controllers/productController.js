c9e0c10df54d369bb2ccfa37da7d813d1b51ce8a
kevin kalenga <kevinkalenga10@gmail.com>
2025-10-28 16:06:50 +0100

    Gestion des erreurs asynchrones

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,26 +1,27 @@
 import Product from "../models/productModel.js"
 import ErrorHandler from '../utils/errorHandler.js'
+import catchAsyncErrors from "../middleware/catchAsyncErrors.js"
 
 //Créer le produit => /api/v1/admin/products
-export const newProduct = async (req, res) => {
+export const newProduct = catchAsyncErrors(async (req, res) => {
    const product = await Product.create(req.body)
    res.status(200).json({
     product
    })
-}
+})
 
 //Obtenir tous les produits  => /api/v1/products
 
-export const getProducts = async (req, res) => {
+export const getProducts = catchAsyncErrors(async (req, res) => {
     const products = await Product.find()
 
     res.status(200).json({
         products
     })
-}
+})
 //Obtenir un produit à partir de son id  => /api/v1/products/:id
 
-export const getProductDetails = async (req, res, next) => {
+export const getProductDetails = catchAsyncErrors(async (req, res, next) => {
     
      const product = await Product.findById(req?.params.id)
     
@@ -34,11 +35,11 @@ export const getProductDetails = async (req, res, next) => {
     res.status(200).json({
         product
     })
-}
+})
 
 // Mettre à jour un produit (Admin) => /api/v1/products/:id
 
-export const updateProduct = async (req, res, next) => {
+export const updateProduct = catchAsyncErrors(async (req, res, next) => {
     let product = await Product.findByIdAndUpdate(req?.params.id, req.body, { new: true});
     
      if(!product) {
@@ -48,8 +49,8 @@ export const updateProduct = async (req, res, next) => {
     res.status(200).json({
         product,
     })
-}
-export const deleteProduct = async (req, res, next) => {
+})
+export const deleteProduct = catchAsyncErrors(async (req, res, next) => {
     let product = await Product.findById(req?.params.id);
     
      if(!product) {
@@ -59,4 +60,4 @@ export const deleteProduct = async (req, res, next) => {
     res.status(200).json({
         message: "Produit supprimé"
     })
-}
+})
