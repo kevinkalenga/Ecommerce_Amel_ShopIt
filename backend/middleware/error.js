d20f5c8bdab79524f88ec57f869a58e0f98dcade
kevin kalenga <kevinkalenga10@gmail.com>
2025-10-28 15:53:37 +0100

    Gestion d'erreur en dev et en prod

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -8,6 +8,23 @@ export default (err, req, res, next) => {
         message: err?.message || "Internal server error"
     }
 
+    if(process.env.NODE_ENV === 'DEVELOPMENT') {
+        res.status(error.statusCode).json({
+            message: error.message,
+            error: err,
+            stack: err?.stack
+        })
+    }
+    if(process.env.NODE_ENV === 'PRODUCTION') {
+        res.status(error.statusCode).json({
+            message: error.message,
+          
+        })
+    }
+    
+    
+    
+    
     res.status(error.statusCode).json({
         message: error.message
     })
