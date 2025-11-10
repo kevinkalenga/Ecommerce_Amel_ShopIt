30a06bdca961c0369baac76854d35afca9ec9ed4
kevin kalenga <kevinkalenga10@gmail.com>
2025-11-10 16:12:13 +0100

    Creation des fonction permettant de recuperer toutes les commandes et leurs details

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,11 @@
 import express from "express"
 import {isAuthenticatedUser} from "../middleware/auth.js"
-import {newOrder} from "../controllers/orderController.js"
+import {newOrder, getOrderDetails, myOrders} from "../controllers/orderController.js"
 
 const router = express.Router();
 
 router.route('/orders/new').post(isAuthenticatedUser, newOrder)
+router.route('/orders/:id').get(isAuthenticatedUser, getOrderDetails)
+router.route('/me/orders').get(isAuthenticatedUser, myOrders)
 
 export default router;
