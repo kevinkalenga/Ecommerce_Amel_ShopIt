3e013540075f76d69047de1806a3b01cf84e2ecf
kevin kalenga <kevinkalenga10@gmail.com>
2025-11-17 15:32:23 +0100

    Création de la homePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import './App.css'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-       <Header />
-       <Footer />
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+
+        <Footer />
     </div>
   );
 }
