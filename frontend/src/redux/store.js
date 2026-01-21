5535c68131dc5cb14576c34a5f71303ea5bbb7d3
kevin kalenga <kevinkalenga10@gmail.com>
2026-01-21 15:45:43 +0100

    Gestion de Add to cart button

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,10 +3,12 @@ import { productApi } from './api/productsApi';
 import {authApi} from './api/authApi';
 import userReducer from "./features/userSlice"
 import { userApi } from './api/userApi';
+import cartReducer from './features/cartSlice'
 
 export const store = configureStore({
     reducer: {
         auth:userReducer,
+        cart:cartReducer,
         [productApi.reducerPath]:productApi.reducer,
         [authApi.reducerPath]:authApi.reducer,
         [userApi.reducerPath]:userApi.reducer,
