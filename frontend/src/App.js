f30f71d1a64f57ab42c0a0c607a6711fc75b558a
kevin kalenga <kevinkalenga10@gmail.com>
2026-01-26 16:36:48 +0100

    Confirm order detail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import ForgotPassword from './components/auth/ForgotPassword'
 import ResetPassword from './components/auth/ResetPassword'
 import Cart from './components/cart/Cart'
 import Shipping from './components/cart/Shipping'
+import ConfirmOrder from './components/cart/ConfirmOrder'
 
 function App() {
   return (
@@ -59,6 +60,11 @@ function App() {
               <Route path='/shipping' element={
                  <ProtectedRoute>
                      <Shipping />
+                 </ProtectedRoute>
+              } />
+              <Route path='/confirm_order' element={
+                 <ProtectedRoute>
+                     <ConfirmOrder />
                  </ProtectedRoute>
               } />
                 <Route path='/cart' element={<Cart />} />
