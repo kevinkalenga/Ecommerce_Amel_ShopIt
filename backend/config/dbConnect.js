948abcefc2016b480b0003fd4f3d7d010f629db1
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-08 10:29:14 +0200

    bug fixed 2

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,12 +1,27 @@
+// import mongoose from "mongoose";
+
+// export const connectedDatabase = () => {
+//    let DB_URI = ""
+
+//    if(process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_URI_LOCAL;
+//    if(process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
+//    mongoose.connect(DB_URI).then((con) => {
+//       console.log(`Connexion avec le host: ${con?.connection?.host}`)
+//    })
+
+// }
+
+
 import mongoose from "mongoose";
 
 export const connectedDatabase = () => {
-   let DB_URI = ""
+   const DB_URI = process.env.DB_URI;
 
-   if(process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_URI_LOCAL;
-   if(process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
+   if (!DB_URI) {
+      throw new Error("DB_URI manquant dans les variables d'environnement");
+   }
+
    mongoose.connect(DB_URI).then((con) => {
-      console.log(`Connexion avec le host: ${con?.connection?.host}`)
-   })
-
-}
+      console.log(`Connexion avec le host: ${con.connection.host}`);
+   });
+};
